8bc520cb90a034ffae0e53d00ff7977b3049959b
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-29 14:44:03 -0500

    Basic boolean tutorial PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/* !Boolean Variables! */
+
+$isReady = true;
+$isReady = false;
+var_dump($isReady);
+
+var_dump(1 + "2");
+
+$a = 10;
+$b = '10';
+
+var_dump($a == $b);//Equality
+var_dump($a === $b);//Identical
+
+$string = 'Hello';
+var_dump($string == 'Hello');//Equality
+
+?>
